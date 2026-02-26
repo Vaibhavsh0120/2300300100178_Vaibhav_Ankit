5b4b1f466ce02d5a6a09252ed9025850431eb0c0
Vaibhavsh0120 <vaibhavsh0120@gmail.com>
2026-02-26 11:46:14 +0530

    DAY 7

---
+++ Day 7 18-02/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,35 @@
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/7270143/best-optimal-sol-beats-similar-to-striver-s-sol
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target< nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(nums[high] >= target && target >= nums[mid]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
