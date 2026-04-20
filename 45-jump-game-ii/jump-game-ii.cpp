0f61c7695caa4603d0cb9813a23ac6ed5d5a4a8a
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-04-21 01:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (32.26%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int dp[10001];
-
-    int solve(vector<int>& nums, int i) {
-
-        if(i >= nums.size() - 1) { // if reach last state stop
-            return 0;
-        }
-
-        if(dp[i] != -1) {  // if already solve for this index, use answer
-            return dp[i];
-        }
-
-        int ans = INT_MAX;  // min number of jumps
-
-        for(int jump = 1; jump <= nums[i]; jump++) { // try all jumps from 1 , 2, ... possible jump from index
-            if(i + jump < nums.size()) {  // Out-of-Bound Safety Net for jumps
-                int res = solve(nums, i + jump); // tried that jump from the index
-
-                if(res != INT_MAX) { // If can reach end from this index
-                    ans = min(ans, 1 + res); // check if curr jump + future jumps are the answer
-                }
-            }
-        }
-
-        return dp[i] = ans;  // save the answer form current jump (if not reachable from here then INT_MAX)
-    }
-
     int jump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp)); // set dp to -1
-        return solve(nums, 0);
+        // GREEDY APPROACH USING RANGES - STRIVER
+        int jumps = 0;
+        int l = 0, r = 0;
+
+        while (r < nums.size() - 1) {
+
+            int farthest = 0; // Farthest from that index
+
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+
+            l = r + 1; // least possible to reach within next jump (which we tried all above)
+            r = farthest; // most possible to reach within next jump
+            // RANGE SET FOR POSSIBLE REACHES FOR NEXT JUMP
+            jumps++; // TAKE JUMP - THEN TRY ALL THE POSSIBLE JUMP RANGE FROM THERE
+        }
+
+        return jumps;
     }
 };
