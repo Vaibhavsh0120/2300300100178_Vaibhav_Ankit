4cba8325f7b34e4f4386291063dd6b6ba7c2176b
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-04-20 11:10:49 +0530

    Time: 14 ms (22.68%) | Memory: 7.7 MB (88.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
-    int simple_fib(int n) {
-        // USING STRIVER DP VID 1
-
-        if(n <= 1) {
-            return n;
-        }
-
-        int prev2 = 0;
-        int prev = 1;
-
-        for(int i = 2 ; i <= n ; i++) {
-            int currFibo = prev + prev2;
-            prev2 = prev;
-            prev = currFibo;
-        }
-
-        return prev;
-    }
-
-    // recusion
     int fib(int n) {
-        if(n <= 1) {
+        if(n == 0 || n == 1) {
             return n;
         }
 
-        return fib(n - 2) + fib(n - 1);
+        return fib(n - 1) + fib(n - 2);
     }
 };
