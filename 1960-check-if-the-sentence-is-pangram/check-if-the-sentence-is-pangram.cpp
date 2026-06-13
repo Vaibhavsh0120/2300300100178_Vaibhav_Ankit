e8974fbbfc773c8daad1a82d03bfafce52f9dd73
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-06-13 16:00:12 +0530

    Time: 3 ms (9.91%) | Memory: 9 MB (37.63%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        set<char> st;
+
+        for(char c : sentence) {
+            st.insert(c);
+        }
+
+        if(st.size() == 26) {
+            return true;
+        }
+
+        return false;
+    }
+};
