73c4d577c61de4f416516fff9de177875e3e5923
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-07-24 14:55:43 +0530

    Time: 29 ms (13.82%) | Memory: 14.8 MB (20.60%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool permutationCheck(unordered_map<char, int>& original, unordered_map<char, int>& mixed) {
+        for(auto kv : original) {
+            if(original[kv.first] != mixed[kv.first]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // EXACTLY Permutation in String
+    vector<int> findAnagrams(string s1, string s2) {
+        vector<int> soln;
+
+        swap(s1, s2);
+
+        int windowSize = s1.size();
+
+        unordered_map<char, int> original, mixed;
+
+        for(int i = 0 ; i < s1.size() ; i++) {
+            original[s1[i]]++;
+        }
+
+        int maxSize = 0;
+        int l = 0;
+
+        for(int r = 0 ; r < s2.size() ; r++) {
+            // add
+            mixed[s2[r]]++;
+
+            // if out of bound window
+            if(r - l + 1 > windowSize) {
+                mixed[s2[l]]--;
+                if(mixed[s2[l]] == 0) {
+                    mixed.erase(s2[l]);
+                }
+
+                l++;
+            }
+
+            // reached window size
+            if(r - l + 1 == windowSize) {
+                // both window have same frequency elements -- Permutation in String found!!
+                if(permutationCheck(original, mixed)) {
+                    soln.push_back(l);
+                }
+            }
+        }
+
+        return soln;
+    }
+};
