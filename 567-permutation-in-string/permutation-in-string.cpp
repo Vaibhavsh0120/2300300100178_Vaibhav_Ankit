c1949fa05804c285cca73dc83c3904f5d2232203
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-07-24 13:57:24 +0530

    Time: 335 ms (8.63%) | Memory: 144.6 MB (5.34%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool permutationCheck(unordered_map<char, int> original, unordered_map<char, int> mixed) {
+        for(auto kv : original) {
+            if(original[kv.first] != mixed[kv.first]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int windowSize = s1.size();
+
+        unordered_map<char, int> original, mixed;
+
+        for(int i = 0 ; i < s1.size() ; i++) {
+            original[s1[i]]++;
+        }
+
+        int maxSize = 0;
+        int l = 0;
+
+        for(int r = 0 ; r < s2.size() ; r++) {
+            // add
+            mixed[s2[r]]++;
+
+            // if out of bound window
+            if(r - l + 1 > windowSize) {
+                mixed[s2[l]]--;
+                if(mixed[s2[l]] == 0) {
+                    mixed.erase(s2[l]);
+                }
+
+                l++;
+            }
+
+            // reached window size
+            if(r - l + 1 == windowSize) {
+                // both window dont have same frequency elements
+                if(permutationCheck(original, mixed)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+};
