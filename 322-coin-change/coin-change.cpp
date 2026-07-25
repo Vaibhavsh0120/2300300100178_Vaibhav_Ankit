1fbc066d56695a22b3bfbd63d06374c674db5c0b
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-07-25 11:33:15 +0530

    Time: 59 ms (14.51%) | Memory: 49.7 MB (22.58%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        // ------- BASE CONDITION
-        if(amount == 0) {   // reach result
+    vector<vector<int>> dp;
+
+    int solve(int i, vector<int>& coins, int amount) {
+        // base c
+        if(i == coins.size()) {
+            return 99999;
+        }
+
+        if(amount == 0) {
             return 0;
         }
 
-        if(i == coins.size()) { // Out of type of coins
-            return 99999;   // making sure this sequence is not min
-        }
-        
-        // ------- DP
+        // dp
         if(dp[i][amount] != -1) {
             return dp[i][amount];
         }
 
-        // -------- TAKE , NOT TAKE
-        int take = 99999;   // make sure if statement is not executed, take is not min
-        if(0 <= amount - coins[i]) {
-            take = 1 + solve(i, coins, amount - coins[i], dp);
+        // working
+        int take = 99999;
+        if(coins[i] <= amount) {
+            take = 1 + solve(i, coins, amount - coins[i]);
         }
 
-        int nottake = solve(i + 1, coins, amount, dp);
+        int notTake = solve(i + 1, coins, amount);
 
-        // -------- RESULT
-        return dp[i][amount] = min(take, nottake);
-        
+        return dp[i][amount] = min(take, notTake);
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        
-        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
+        dp.assign(coins.size(), vector<int>(amount+1, -1));
 
-        int ans = solve(0, coins, amount, dp);
-
-        if(ans == 99999) {
-            return -1;
-        }
-        return ans;
+        int ans = solve(0, coins, amount);
+        return ans >= 99999 ? -1 : ans;
     }
 };
