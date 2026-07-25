5735300574ad67b74d170cba6a5f346b62f9642b
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2026-07-25 11:39:45 +0530

    Time: 52 ms (13.18%) | Memory: 36.9 MB (27.86%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
     vector<vector<int>> dp;
 
-    int  helper(int i, int amount, vector<int> coins) {
-        // correct ans
+    int solve(int i, int amount, vector<int>& coins) {
+        // base c
         if(amount == 0) {
             return 1;
         }
 
-        // end of array
         if(i == coins.size()) {
             return 0;
         }
 
-        // memo
-        if (dp[i][amount] != -1) {
+        // dp
+        if(dp[i][amount] != -1) {
             return dp[i][amount];
         }
 
-        // if curr coin can be used accept and try this coin again
+        // working
         int take = 0;
         if(coins[i] <= amount) {
-            take = helper(i, amount - coins[i], coins);
+            take = solve(i, amount - coins[i], coins);
         }
 
-        // try next coin
-        int notTake = helper(i+1, amount, coins);
+        int notTake = solve(i + 1, amount, coins);
 
         return dp[i][amount] = take + notTake;
     }
 
     int change(int amount, vector<int>& coins) {
-        dp.assign(coins.size(), vector<int>(amount + 1, -1));
-        return helper(0, amount, coins);
+        dp.assign(coins.size() + 1, vector<int>(amount + 1, -1));
+        return solve(0, amount, coins);
     }
 };
